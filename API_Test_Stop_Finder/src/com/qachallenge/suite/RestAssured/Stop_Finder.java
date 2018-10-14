4fd1c6144ab8c3c856d79a08aa3b92e055c236a4
feature/Daniel-Fernandes-QloyalCodeTest <danferns37@gmail.com>
2018-10-14 19:35:44 +1100

    Update to the API test

--- API_Test_Stop_Finder/src/com/qachallenge/suite/RestAssured/Stop_Finder.java
+++ API_Test_Stop_Finder/src/com/qachallenge/suite/RestAssured/Stop_Finder.java
@@ -29,14 +29,14 @@ import io.restassured.config.EncoderConfig;
 
 public class Stop_Finder {
 	
-	
+	//Url Initialization
 	@BeforeTest
 	public void setup() {
 	    RestAssured.baseURI = "https://www.transportnsw.info";
-	    //RestAssured.port = 443;
+	    
 	}
 
-	// Verification of Initial Request
+	// Verification of status of the get request on transport nsw api
 	@Test
 	public void verifystatus() {
 		
@@ -52,7 +52,7 @@ public class Stop_Finder {
 			.statusCode(200).log().all();
 	}
 
-	
+	//verification of the location ,stop and modes of transport
 	@Test
 	public void verifystopsmodes() throws InterruptedException {
 	
@@ -68,7 +68,7 @@ public class Stop_Finder {
 		.body("locations[0].assignedStops[0].modes",hasItems(1,5,11));
 	}
 
-	
+	//validation of api response time
 	@Test
 	public void whenValidateResponseTime_thenSuccess() {
 	    when().get("/web/XML_STOPFINDER_REQUEST?TfNSWSF=true&language=en%20&name_sf=Wynyard%20Station&outputFormat=rapidJSON&type_sf=any&version=10.2.2.48").then().time(lessThan(5000L));
